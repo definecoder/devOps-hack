03917f4f5dba961ec1a4836d66ac1d121e062493
codermehraj <codermehraj@gmail.com>
2024-03-02 13:11:53 +0600

    basic tracing done

--- node2/app.js
+++ node2/app.js
@@ -12,6 +12,25 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// TELEMETRY //////////////////////////////////////////
+const configureOpenTelemetry = require("./tracing");
+const { trace, context, propagation } = require("@opentelemetry/api");
+const tracerProvider = configureOpenTelemetry("node-2-heavy"); // add service name here
+
+app.use((req, res, next) => {
+  const tracer = tracerProvider.getTracer("express-tracer");
+  const span = tracer.startSpan("node-2-root-span");
+
+  // Add custom attributes or log additional information if needed
+  span.setAttribute("team name", "SUST Define Coders");
+
+  // Pass the span to the request object for use in the route handler
+  context.with(trace.setSpan(context.active(), span), () => {
+    next();
+  });
+});
+// TELEMETRY //////////////////////////////////////////
+
 const PORT = process.env.PORT || 7080;
 
 app.get("/get-posts/:id", getPostsExceptCurrentUser);
@@ -21,3 +40,19 @@ app.post("/create-post", createPost);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// Gracefully shut down the OpenTelemetry SDK and the server
+const gracefulShutdown = () => {
+  server.close(() => {
+    console.log("Server stopped");
+    sdk
+      .shutdown()
+      .then(() => console.log("Tracing terminated"))
+      .catch((error) => console.error("Error shutting down tracing", error))
+      .finally(() => process.exit(0));
+  });
+};
+
+// Listen for termination signals
+process.on("SIGTERM", gracefulShutdown);
+process.on("SIGINT", gracefulShutdown);

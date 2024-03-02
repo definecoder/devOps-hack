e27f494da35db20736aa395a1616a74dfd34e76e
shawon-majid <shawon.majid@gmail.com>
2024-03-02 13:29:57 +0600

    mysql automated

--- node2/app.js
+++ node2/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const fs = require("fs");
 dotenv.config();
 require("./database/mongodb");
 const {
@@ -8,9 +9,33 @@ const {
   getPostsExceptCurrentUser,
   getAllPosts,
 } = require("./controllers/feeds");
+const { pool } = require("./database/mysql");
+
+// Read the SQL file
+const sqlFilePath = "./db.sql"; // Update the path to your SQL file
+const sqlQueries = fs.readFileSync(sqlFilePath, "utf8");
+const statements = sqlQueries.split(";");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// pool.query(sqlQueries, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Database schema has been created");
+//   }
+// });
+statements.forEach((statement) => {
+  if (statement) {
+    pool.query(statement, (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
+console.log("Database schema has been created");
 
 const PORT = process.env.PORT || 7080;
 
